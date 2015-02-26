1a938d09844845a87ec58933500d51c42e03003c
talentlei <chenlei_0630@163.com>
2015-02-26 08:58:17 +0800

    #47
    #47

---
+++ python/41-50/Permutations II.py
@@ -0,0 +1,20 @@
+    # @param num, a list of integer
+    # @return a list of lists of integers
+    def permuteUnique(self, num):
+        ans=[]
+        if not num:
+            return ans
+        num.sort()
+        pos=0
+        while pos<len(num):
+            num[0],num[pos]=num[pos],num[0]
+            myset=self.permuteUnique(num[1:])
+            if myset:
+                ans+=[[num[0]]+lis for lis in myset]
+            else:
+                ans+=[[num[0]]]
+            num[0],num[pos]=num[pos],num[0]
+            while pos+1 <len(num) and num[pos]==num[pos+1]:
+                pos+=1
+            pos+=1
+        return ans   
