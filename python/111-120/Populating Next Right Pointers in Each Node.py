2413902cf5d7e319403724cf1bc4992e84f51174
talentlei <chenlei_0630@163.com>
2015-03-07 16:28:19 +0800

    #116
    #116

---
+++ python/111-120/Populating Next Right Pointers in Each Node.py
@@ -0,0 +1,23 @@
+    # @param root, a tree link node
+    # @return nothing
+    def connect(self, root):
+        if root ==None:
+            return
+        upper,down=[],[]
+        upper.append(root)
+        while len(upper)!=0:
+            i=0
+            pre = TreeLinkNode(0)
+            while i<len(upper):
+                node=upper[i]
+                pre.next=node
+                pre = node
+                if node.left!=None:
+                    down.append(node.left)
+                if node.right!=None:
+                    down.append(node.right)
+                i+=1
+            upper=[]
+            if len(down)>0:
+                upper=down[:]
+                down=[]
