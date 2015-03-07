cb697f3eda9c927a40c4a8c36e6018b235d80549
talentlei <chenlei_0630@163.com>
2015-03-07 12:31:31 +0800

    #107
    #107

---
+++ python/101-110/Binary Tree Level Order Traversal II.py
@@ -0,0 +1,26 @@
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrderBottom(self, root):
+        if root ==None:
+            return []
+        result=[]
+        upper,down=[],[]
+        upper.append(root)
+        while len(upper)!=0:
+            i=0
+            temp=[]
+            while i<len(upper):
+                node=upper[i]
+                temp.append(node.val)
+                if node.left!=None:
+                    down.append(node.left)
+                if node.right!=None:
+                    down.append(node.right)
+                i+=1
+            result+=[temp]
+            upper=[]
+            if len(down)>0:
+                upper=down[:]
+                down=[]
+        result.reverse()
+        return result
