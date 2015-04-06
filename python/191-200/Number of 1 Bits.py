d1b9f829bb7cc1881480552358782ed7a590aaae
talentlei <chenlei_0630@163.com>
2015-04-06 22:36:05 +0800

    #191
    #191

---
+++ python/191-200/Number of 1 Bits.py
@@ -0,0 +1,8 @@
+    # @param n, an integer
+    # @return an integer
+    def hammingWeight(self, n):
+        res=0
+        while n!=0:
+            n=n&(n-1)
+            res+=1
+        return res
