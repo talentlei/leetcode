156dbeaecccc4829565f34c85e8de8982ce9724a
talentlei <chenlei_0630@163.com>
2015-06-08 20:02:44 +0800

    Update Happy Number.py

--- python/201-210/Happy Number.py
+++ python/201-210/Happy Number.py
@@ -17,3 +17,31 @@ class Solution:
                  break
              self.record.append(tmp)
         return tmp==1
+
+##C++ solution
+class Solution {
+public:
+    int getNext(int n){
+        int sum=0;
+        int tmp = n;
+        while(tmp!=0){
+            int re = tmp%10;
+            tmp = tmp/10;
+            sum+=re*re;
+        }
+        return sum;
+        
+    }
+    bool isHappy(int n) {
+        set<int> mySet;
+        int tmp = n;
+        mySet.insert(tmp);
+        while(tmp!=1){
+            tmp = getNext(tmp);
+            if(mySet.count(tmp)!=0)
+                return false;
+            mySet.insert(tmp);
+        }
+        return true;
+    }
+};
