5866c007cce7160a352373d62ac902193cc8ec89
talentlei <chenlei_0630@163.com>
2015-06-13 10:44:38 +0800

    #216
    #216

--- Cpp/211-220/Combination Sum III.cpp
+++ Cpp/211-220/Combination Sum III.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
-    
-    int dfs(vector<int> &nums,vector<int> &base,vector<int > &one,int deep,int k, int n){
+    void dfs(vector<int > &one,int deep,int k, int n,int start){
       if(deep == k){
-        if(n==0){
-            res.push_back(one);
-            return 0;
-          }
-        else if(n>0)
-          return 1;
-        else return -1;
+            if(n==0)
+                res.push_back(one);
+            return ;
       }
-      if(n<0)
-        return -1;
-      for(int i=0; i<nums.size(); i++){
-        if(base[i] == 1)
-          continue;
-        base[i] = 1;
-        one.push_back(i+1);
-        int flag = dfs(nums,base,one,deep+1,k,n-i-1);
-        base[i] = 0;
-        one.pop_back();
-        if(flag<0)
-          break;
+      for(int i=start; i<9; i++){
+        one[deep]=i+1;
+        dfs(one,deep+1,k,n-i-1,i+1);
       }
-  
+      return;
     }
-    vector<vector<int>> combinationSum3(int k, int n) {
-          if(k>9||k<=0||k>n||n>45)
+    vector<vector<int> > combinationSum3(int k, int n) {
+          if(k>9||k<=0||k>n||n>9*k)
             return res;
-          vector<int > nums(9);
-          vector<int > base(9);
-          for(int i=1; i<=9;i++){
-            nums[i-1] = i;
-            base[i-1] = 0;
-            }
-          sort(nums.begin(),nums.end());
-          vector<int> one;
-          dfs(nums,base,one,0,k,n);
+          vector<int> one(k,0);
+          dfs(one,0,k,n,0);
+          return res;
     }
 private:
     vector<vector<int> > res;
