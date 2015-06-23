d359ee98ae7dad2d2329fac01f2208edffc36719
talentlei <chenlei_0630@163.com>
2015-06-23 13:11:06 +0800

    #153
    #153

---
+++ Cpp/151-160/Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if(nums.size()<2||nums[0]<nums[nums.size()-1])
+          return nums[0];
+        int beg = 0,end = nums.size()-1;
+        while(beg<end){
+          int mid = beg+(end-beg)/2;
+          if(nums[mid]>nums[mid+1])
+            return nums[mid+1];
+          else if(nums[mid]<nums[0])
+            end = mid-1；
+          else beg = mid+1;
+        }
+        return nums[beg];
+    }
+};
