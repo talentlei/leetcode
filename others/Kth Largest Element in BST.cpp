2bc9abf4fb3243876f4acd2d004206be0766984e
talentlei <chenlei_0630@163.com>
2015-06-23 21:24:09 +0800

    Update Kth Largest Element in BST.cpp

--- others/Kth Largest Element in BST.cpp
+++ others/Kth Largest Element in BST.cpp
@@ -1,11 +1,12 @@
 /*
  find kth largest element in binary search tree
 */
-struct {
+typedef struct TreeNode{
   int val;
   TreeNode * left;
   TreeNode * right;
-}TreeNode;
+  TreeNode(int x): val(x),left(NULL),right(NULL){};
+};
 class Solution{
   int getNum(TreeNode * root){
     if(root==NULL)
@@ -19,12 +20,11 @@ class Solution{
     int lk = num-k+1;
     int cnt=0;
     stack<TreeNode*> myStack;
-    myStack.push(root);
     TreeNode* temp = root;
-    while(！myStack.empty()){
+    while(!myStack.empty()||temp!=NULL){
         while(temp!=NULL){
+         myStack.push(temp);
           temp = temp->left;
-          myStack.push(temp);
         }
         temp = myStack.top();
         myStack.pop();
@@ -34,5 +34,40 @@ class Solution{
         temp = temp->right;
     }
     return temp->val;
+  }
+};
+
+
+/*
+ find kth largest element in binary search tree
+ recursion method. it contain repeat computing. 
+ to improve this， you can store some userful infor.
+ 
+*/
+typedef struct TreeNode{
+  int val;
+  TreeNode * left;
+  TreeNode * right;
+  int lcnt;
+  int rcnt;
+  TreeNode(int x): val(x),left(NULL),right(NULL){},lcnt(0),rcnt(0);
+};
+class Solution{
+  int solve(TreeNode * root,int k){
+   getNum(root);
+   return getKthLargest(root,k);
+  }
+  void getNum(TreeNode * root){
+    if(root==NULL)
+      return 0;
+    root->lcnt = getNum(root->left);
+    root->rcnt = getNum(root->right);
+  }
+  int getKthLargest(TreeNode * root,int k){
+    if(root->rcnt == k-1)
+     return root->val;
+    if(root->rcnt >k-1)
+     return getKthLargest(root->right, k);
+    else return getKthLargest(root->left,k-root->rcnt-1);
   }
 };
