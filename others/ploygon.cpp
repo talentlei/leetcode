9a950946caff896529c2305bf08d24d8c33c4667
talentlei <chenlei_0630@163.com>
2015-06-23 17:08:54 +0800

    Create ploygon.cpp

---
+++ others/ploygon.cpp
@@ -0,0 +1,16 @@
+/*
+  given a array of points, check if they can construct ploygon line the points by order.
+*/
+
+class Solution{
+public :
+  // it can construct ploygon 
+  //1 .if every per two line doesn't intersect
+  //2 . closet graph
+  bool isPloygon(vector<pair<int,int> > points){
+    
+  
+  }
+
+
+}
