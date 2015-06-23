56c74a5c6d12f8d030343f28b2bd4864f7e64247
talentlei <chenlei_0630@163.com>
2015-06-23 17:38:20 +0800

    #148
    #148

---
+++ Cpp/141-150/sortList.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+ // quick sort
+class Solution1 {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
+
+// merge sort
+class Solution2 {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
+
+//insert sort
+class Solution3 {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
+
+//radix sort
+class Solution4 {
+public:
+    ListNode* sortList(ListNode* head) {
+        
+    }
+};
