d085fe6f86fe1a5097ba3f1ced83f230690a24cc
talentlei <chenlei_0630@163.com>
2015-06-24 23:17:35 +0800

    #164
    #164

---
+++ Cpp/161-170/Maximum Gap.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        int size=nums.size();
+        if(size<2) return 0;
+        int maxe = *max_element(nums.begin(),nums.end());
+        int mine = *min_element(nums.begin(),nums.end());
+        int gap = (maxe-mine)/(size-1)+1;
+        int Max[size-1] = {-1};
+        int Min[size-1] = {maxe};
+        for(int i=0; i<size; i++){
+          if(nums[i]==maxe)
+            continue;
+          int BucketId = (nums[i]-mine)/gap;
+          Max[BucketId] = max(Max[BucketId],nums[i]);
+          Min[BucketId] = min(Min[BucketId],nums[i]);
+        }
+        int t=mine,res=0;
+        for(int i=0; i<size-1; i++){
+          if(Min[i] == maxe)
+            continue;
+          if(Min[i]-t>res)
+            res = Min[i]-t;
+          t = Max[i];
+        }
+        if(maxe-t>res)
+          res = maxe-t;
+        return res;
+    }
+    
+};
