b285a95a4622509f37bea686eaa7a26709737e50
talentlei <chenlei_0630@163.com>
2015-06-28 23:51:14 +0800

    #86
    #86

--- Cpp/81-90/Partition List.cpp
+++ Cpp/81-90/Partition List.cpp
@@ -9,6 +9,20 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        
+        ListNode thead(0);
+        thead.next = head;
+        ListNode* temp = &thead,*pre =&thead;
+        while(temp->next!=nullptr){
+            if(temp->next->val>=x)
+                temp = temp->next;
+            else{
+                ListNode* t = temp->next;
+                temp->next =t->next;
+                t->next = pre->next;
+                pre->next = t;
+                pre = t;
+            }
+        }
+        return thead.next;
     }
 };
