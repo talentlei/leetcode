aeef9b5001ec73274fb6aaa37f0d752c0b0d581c
talentlei <chenlei_0630@163.com>
2015-06-28 21:12:48 +0800

    Update Substring with Concatenation of All Words.cpp

--- Cpp/21-30/Substring with Concatenation of All Words.cpp
+++ Cpp/21-30/Substring with Concatenation of All Words.cpp
@@ -1,6 +1,40 @@
 class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
-        
+        vector<int > res;
+        if(words.size()==0||s.size()==0)
+            return res;
+        unordered_map<string,int> mySet;
+        unordered_map<string,int> wordsnum;
+        for(int i=0; i<words.size(); i++)
+            wordsnum[words[i]]++;
+        int len = words[0].size();
+        int num=0;
+        for(int beg = 0; beg<len; beg++){
+            int i = beg;
+            int bpos;
+            for(;i<s.size(); i+=len){
+                string word = s.substr(i,len);
+                if(words.find(word)==words.end())
+                    mySet.clear();
+                else{
+                    if(mySet.find(word)==mySet.end()){
+                        if(mySet.size()==0)
+                            bpos = i;       
+                        mySet.insert(word);
+                        num++;
+                    }
+                    else if(mySet[word]<wordsnum[word]){
+                        num++;
+                        mySet[word]++;
+                    }else{
+                        string bw; 
+                        while(bw = s.substr(bpos,len)!=word)
+                            bpos
+                    }
+                        
+                }
+            }
+        }
     }
 };
