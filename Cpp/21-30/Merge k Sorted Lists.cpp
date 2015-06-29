3bed09e3936e2671ca801cbe4f0250142433afb2
talentlei <chenlei_0630@163.com>
2015-06-30 00:26:28 +0800

    #23
    #23

--- Cpp/21-30/Merge k Sorted Lists.cpp
+++ Cpp/21-30/Merge k Sorted Lists.cpp
@@ -42,4 +42,26 @@ public:
         else temp->next = l1;
         return head.next;
     }
+    
+    
 };
+
+//一个写法上更简单的方法，利用priority_queue
+   struct cmp {
+        bool operator()(const ListNode* a, const ListNode* b) { return a->val > b->val; }
+    };
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> q;
+        ListNode h(0), *t=&h;
+        for (auto& list : lists) {
+            if (list) q.push(list);
+        }
+        while(!q.empty()) {
+            ListNode* cur = q.top();
+            q.pop();
+            if (cur->next) q.push(cur->next);
+            t = t->next = cur;
+        }
+        t->next = NULL;
+        return h.next;
+    }
