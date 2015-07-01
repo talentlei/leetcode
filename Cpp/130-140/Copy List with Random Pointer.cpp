4007a61d82408e1d67a0e5108de74c910326fe4a
talentlei <chenlei_0630@163.com>
2015-07-01 23:25:29 +0800

    #138
    #138

--- Cpp/130-140/Copy List with Random Pointer.cpp
+++ Cpp/130-140/Copy List with Random Pointer.cpp
@@ -9,6 +9,25 @@
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
-        
+        if(head==NULL) return NULL;
+        RandomListNode* temp=head;
+        while(temp!=NULL){
+            RandomListNode *t = new RandomListNode(temp->label);
+            t->next = temp->next;
+            temp->next=t;
+            temp = temp->next->next;
+        }
+        RandomListNode thead(0);
+        temp = &thead;
+        RandomListNode* cur1;
+        for(cur1=head; cur1; cur1=cur1->next->next) {
+      cur1->next->random = cur1->random? cur1->random->next:NULL;
+    }
+    for(cur1=head; cur1; cur1=cur1->next) {
+      temp = temp->next = cur1->next;
+      cur1->next = temp->next;
+    }
+         temp->next = NULL;
+        return thead.next;
     }
 };
