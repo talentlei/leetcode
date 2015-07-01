9c24083b2f38d2f7dad325d05f4ebc83064e4b17
talentlei <chenlei_0630@163.com>
2015-07-01 23:57:49 +0800

    #20
    #20

--- Cpp/11-20/Valid Parentheses.cpp
+++ Cpp/11-20/Valid Parentheses.cpp
@@ -1,6 +1,24 @@
+/*
+    runtime:0ms
+    error:1
+    匹配右括号时忘记检查stack是否为空
+*/
 class Solution {
 public:
     bool isValid(string s) {
-        
+        stack<char> myStack;
+        map<char,char> myMap;
+        myMap[')']='(';
+        myMap[']']='[';
+        myMap['}']='{';
+        for(int i=0; i<s.size(); i++){
+            char ch = s[i];
+            if(ch =='('||ch =='['||ch =='{')
+                myStack.push(ch);
+            else if(!myStack.empty()&&myStack.top()==myMap[ch])
+                myStack.pop();
+            else return false;
+        }
+        return myStack.empty();
     }
 };
