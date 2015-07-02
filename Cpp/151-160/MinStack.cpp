cf320900bed4dece6ad04e679c91fa134007e384
talentlei <chenlei_0630@163.com>
2015-07-02 22:12:12 +0800

    #155
    #155

--- Cpp/151-160/MinStack.cpp
+++ Cpp/151-160/MinStack.cpp
@@ -1,18 +1,38 @@
+/*
+    runtime: 40ms
+    error : 0
+*/
+
 class MinStack {
 public:
+    
     void push(int x) {
-        
+        myStack.push_back(x);
+        pos++;
+        if(pos>0&&x<Min[pos-1]||pos==0)
+            Min.push_back(x);
+        else
+            Min.push_back(Min[pos-1]);
     }
 
     void pop() {
-        
+        if(pos==-1) return;
+        myStack.pop_back();
+        Min.pop_back();
+        pos--;
     }
 
     int top() {
-        
+        if(pos==-1) return -1;
+        return myStack[pos];
     }
 
     int getMin() {
-        
+        if(pos==-1) return -1;
+        return Min[pos];
     }
+private:
+    vector<int> myStack;
+    vector<int> Min;
+    int pos = -1;
 };
