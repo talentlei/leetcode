72b79e5eb4ea14f289d0690044679e2bf8ddde60
talentlei <chenlei_0630@163.com>
2015-07-03 15:34:55 +0800

    #103
    #103

--- Cpp/101-110/Binary Tree Zigzag Level Order Traversal.cpp
+++ Cpp/101-110/Binary Tree Zigzag Level Order Traversal.cpp
@@ -7,9 +7,38 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+ /*
+    runtime: 4ms
+    error: 0
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        
+        vector<vector<int> > res;
+        if(root==NULL) return res;
+        stack<TreeNode*> myStack;
+        stack<TreeNode*> temp;
+        int isleft=1;
+        myStack.push(root);
+        while(!myStack.empty()){
+            vector<int> one;
+            while(!myStack.empty()){
+                TreeNode* node = myStack.top();
+                myStack.pop();
+                one.push_back(node->val);
+                if(isleft==1){
+                    if(node->left!=NULL)  temp.push(node->left);
+                    if(node->right!=NULL)  temp.push(node->right);
+                }else{
+                    if(node->right!=NULL) temp.push(node->right);
+                    if(node->left!=NULL)  temp.push(node->left);
+                }
+            }
+            isleft*=-1;
+            myStack.swap(temp);
+            res.push_back(one);
+        }
+        return res;
     }
 };
