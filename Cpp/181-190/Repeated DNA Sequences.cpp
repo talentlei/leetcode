7277818690b544759107050d2e44e673c9270e2d
talentlei <chenlei_0630@163.com>
2015-07-03 23:02:02 +0800

    Update Repeated DNA Sequences.cpp

--- Cpp/181-190/Repeated DNA Sequences.cpp
+++ Cpp/181-190/Repeated DNA Sequences.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-    vector<string> findRepeatedDnaSequences(string s) {
-        
+/**
+ *  暴力方法: tle
+ * 
+ */ 
+vector<string> findRepeatedDnaSequences(string s) {
+        vector<string> res;
+        if(s.size()<20) return res;
+        unordered_set<string> model;
+        for(int i=0; i+20<=s.size(); i++){
+            string p = s.substr(i,10);
+            if(model.find(p)!=model.end())
+                continue;
+            for(int j=i+10; j+10<=s.size(); j+=1){
+                if(s.substr(j,10)==p){
+                    model.insert(p);
+                    res.push_back(p);
+                    break;
+                }
+            }
+        }
+        return res;
     }
+    
 };
