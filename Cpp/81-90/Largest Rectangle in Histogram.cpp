22e64ce21a986f8e61f50965c13e761401b636c8
talentlei <chenlei_0630@163.com>
2015-07-03 14:52:51 +0800

    #84
    #84

--- Cpp/81-90/Largest Rectangle in Histogram.cpp
+++ Cpp/81-90/Largest Rectangle in Histogram.cpp
@@ -1,6 +1,26 @@
+/*
+    runtime:24ms
+    error: 1  
+    h = myStack.top() should be h = height[myStack.top()];
+
+*/
 class Solution {
 public:
     int largestRectangleArea(vector<int>& height) {
-        
+        stack<int> myStack;
+        if(height.size()==0) return 0;
+        height.push_back(0);
+        int Max=0;
+        for(int i=0; i<height.size(); i++){
+            while(!myStack.empty()&&height[i]<height[myStack.top()]){
+                int h=height[myStack.top()];
+                myStack.pop();
+                int wid = myStack.empty()?-1:myStack.top();
+                int Area = (i-1-wid)*h;
+                if(Area>Max) Max = Area;
+            }
+            myStack.push(i);
+        }
+        return Max;
     }
 };
