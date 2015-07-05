148ee0b0e9b4076c00e7a17e64a3a649b79a10c3
talentlei <chenlei_0630@163.com>
2015-07-05 18:02:09 +0800

    #187
    #187

--- Cpp/181-190/Repeated DNA Sequences.cpp
+++ Cpp/181-190/Repeated DNA Sequences.cpp
@@ -22,5 +22,48 @@ vector<string> findRepeatedDnaSequences(string s) {
         }
         return res;
     }
-    
+ /*
+        将字符串hash到数字
+ */
+ vector<string> findRepeatedDnaSequences(string s) {
+         vector<string> res;
+         if(s.size()<10) return res;
+         unordered_set<unsigned int> mySet;
+         unordered_set<unsigned int> visited;
+         int i=0;
+         unsigned int temp=0;
+         while(i<9){
+                 temp=temp<<3|s[i]&0X07;
+                 i++;
+         }
+         while(i<s.size()){
+                 temp = temp<<3&0X03FFFFFFF|s[i]&0X07;
+                 if(mySet.find(temp)!=mySet.end()){
+                        if(visited.find(temp)==visited.end()){
+                                res.push_back(s.substr(i-9,10));
+                                visited.insert(temp);
+                        }
+                 }
+                else mySet.insert(temp);
+                i++;
+         }
+         return res;
+ }
+ /*
+        字符串加入到hash
+        MLE
+ */
+  vector<string> findRepeatedDnaSequences(string s) {
+         vector<string> res;
+         if(s.size()<10) return res;
+         unordered_map<string,int> myMap;
+         int i=0;
+         while(i+10<=s.size()){
+                string word = s.substr(i,i+10);
+                if(myMap.count(word)==1)
+                        res.push_back(word);
+                myMap[word]++;
+         }
+         return res;
+ }
 };
