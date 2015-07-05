c39a4dd0b6c65513352ddd35bb2140e33bd33120
talentlei <chenlei_0630@163.com>
2015-07-05 23:08:31 +0800

    Update Minimum Window Substring.cpp

--- Cpp/71-80/Minimum Window Substring.cpp
+++ Cpp/71-80/Minimum Window Substring.cpp
@@ -1,48 +1,27 @@
     string minWindow(string s, string t) {
-        
+        if(s.size()==0||t.size()==0) return "";
+        unordered_map<char,int> myMap;
+        unordered_map<char,int> temp;
+        foreach(auto ch: t)
+            myMap[ch]++;
+        string res="";
+        int num = 0;
+        int beg=0,end=0;
+        while(end<s.size()){
+            char ch = s[end];
+            if(myMap.find(ch)==myMap.end()){
+                end++;
+                continue;
+            }
+            if(temp[ch]+1==myMap[ch])
+                num++;
+            if(num==myMap.size()){
+                if(res==""||res.size()>end-beg+1)
+                    res = s.substr(beg,end-beg+1);
+                if(myMap.find(s[beg])!=myMap.end()&&temp[s[beg]]==myMap[s[beg]])
+                    num--;
+                beg++;
+            }else end++;
+        }return res;
     }  
   
-   vector<int> findSubstring(string s, vector<string>& words) {
-        vector<int > res;
-        if(words.size()==0||s.size()==0)
-            return res;
-        unordered_map<string,int> mySet;
-        unordered_map<string,int> wordsnum;
-        for(int i=0; i<words.size(); i++)
-            wordsnum[words[i]]++;
-        int len = words[0].size();
-        for(int beg = 0; beg<len; beg++){
-            int num=0, i = beg, bpos;
-            mySet.clear();                           // error 3: forget to clear mySet and num;
-            for(;i+len<=s.size(); i+=len){           //error 2 i+len<s.size();
-                string word = s.substr(i,len);
-                if(wordsnum.find(word)==wordsnum.end()){
-                    mySet.clear();
-                    num=0;           //error 1: forget
-                }
-                else{
-                    if(mySet.find(word)==mySet.end()){
-                        if(mySet.size()==0)
-                            bpos = i;       
-                        mySet[word]++;
-                        num++;
-                    }
-                    else if(mySet[word]<wordsnum[word]){
-                        num++;
-                        mySet[word]++;
-                    }else{
-                        string bw; 
-                        while((bw = s.substr(bpos,len))!=word){
-                            bpos+=len;
-                            mySet[bw]--;
-                            num--;
-                        }
-                        bpos+=len;
-                    }
-                    if(num==words.size())
-                        res.push_back(bpos);
-                }
-            }
-        }
-        return res;
-    }
