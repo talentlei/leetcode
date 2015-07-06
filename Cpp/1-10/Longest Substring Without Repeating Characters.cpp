56b8017e61ec164706c4888ec9eb348070fd50c9
talentlei <chenlei_0630@163.com>
2015-07-06 23:09:33 +0800

    #3
    #3

--- Cpp/1-10/Longest Substring Without Repeating Characters.cpp
+++ Cpp/1-10/Longest Substring Without Repeating Characters.cpp
@@ -1,1 +1,23 @@
 
+/**
+ * 16ms
+ * error: 1  
+ * 
+ * 
+ */ 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int beg=0,Max=0;
+        vector<int> Map(128,-1);
+        for(int i=0; i<s.size(); i++){
+          if(Map[s[i]]>=beg){
+            Max = max(Max,i-beg);
+            beg=Map[s[i]]+1;
+          }
+          Map[s[i]]=i;                //error 1:  add else first time
+        }
+        Max =max(Max,(int)s.size()-beg);
+        return Max;
+    }
+};
